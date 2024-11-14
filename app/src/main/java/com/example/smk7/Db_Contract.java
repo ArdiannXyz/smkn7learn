de71ab8829c490a3f3121d5e027f744e06382c2d
Ardian <163779915+ArdiannXyz@users.noreply.github.com>
2024-11-14 08:32:49 +0700

    menambahkan folder ke vscode

--- app/src/main/java/com/example/smk7/Db_Contract.java
+++ app/src/main/java/com/example/smk7/Db_Contract.java
@@ -2,7 +2,7 @@ package com.example.smk7;
 
 public class Db_Contract {
 
-    public static String ip = "10.10.183.145";
-    public static final String urlLogin = "http://"+ip+"/e_learning/api-login.php";
+    public static String ip = "10.10.182.183";
+    public static final String urlLogin = "http://"+ip+"/WebNewbieTeam//api-login.php";
 
 }
