c83b34eddbf91f4a641583cfeb63a29dfcfaeabc
Ardian <163779915+ArdiannXyz@users.noreply.github.com>
2024-12-20 19:08:00 +0700

    commit

--- app/src/main/java/com/example/smk7/ApiDatabase/Db_Contract.java
+++ app/src/main/java/com/example/smk7/ApiDatabase/Db_Contract.java
@@ -2,7 +2,7 @@ package com.example.smk7.ApiDatabase;
 
 public class Db_Contract {
 
-    public static String ip = "192.168.120.109";
+    public static String ip = "10.0.2.2";
     public static final String BASE_URL = "http://" + ip + "/WebNewbieTeam/";
     public static final String urlLogin = BASE_URL + "api-login.php";
     public static final String urlLupaPassword = BASE_URL + "api-check_email.php";
@@ -11,7 +11,7 @@ public class Db_Contract {
     public static final String urlApiHapusMateri = BASE_URL + "api-hapus_materi.php";
     public static final String urlApiEditMateri = BASE_URL + "api-update_materi.php";
     public static final String urlApiTambahMateri = BASE_URL + "api-tambah_materi.php";
-
+    public static final String urlApiSiswa = BASE_URL + "api_siswa.php";
 
 
     public static final String urlApiUploadMateri = BASE_URL + "api-uploadMateri.php";
