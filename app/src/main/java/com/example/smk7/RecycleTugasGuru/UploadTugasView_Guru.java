12a8650d1c9059ba17eef23af6b42a1cee4e8105
Ardian <163779915+ArdiannXyz@users.noreply.github.com>
2024-12-20 22:03:16 +0700

    commit

--- app/src/main/java/com/example/smk7/RecycleTugasGuru/UploadTugasView_Guru.java
+++ app/src/main/java/com/example/smk7/RecycleTugasGuru/UploadTugasView_Guru.java
@@ -1,7 +1,7 @@
-package com.example.smk7.RecycleTugasGuru;
-
-import androidx.fragment.app.Fragment;
-
-public class UploadTugasView_Guru extends Fragment {
-
-}
+//package com.example.smk7.RecycleTugasGuru;
+//
+//import androidx.fragment.app.Fragment;
+//
+//public class UploadTugasView_Guru extends Fragment {
+//
+//}
